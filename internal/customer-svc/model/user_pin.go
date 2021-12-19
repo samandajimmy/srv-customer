e1187ef8ec34fde1912d25c7892558b21c6d52d1
imamponco <ponco@nusantarabetastudio.com>
2021-12-20 01:01:25 +0700

    [FIXED] Model mapping from External Database camelCase to snakeCase

--- internal/customer-svc/model/user_pin.go
+++ internal/customer-svc/model/user_pin.go
@@ -3,12 +3,12 @@ package model
 import "database/sql"
 
 type UserPin struct {
-	UserId         int64          `db:"userId"`
+	UserId         int64          `db:"user_id"`
 	Cif            sql.NullString `db:"cif"`
-	LastAccessTime sql.NullTime   `db:"lastAccessTime"`
+	LastAccessTime sql.NullTime   `db:"last_access_time"`
 	Counter        int64          `db:"counter"`
-	IsBlocked      int64          `db:"isBlocked"`
-	BlockedDate    sql.NullTime   `db:"blockedDate"`
-	CreatedAt      string         `db:"createdAt"`
-	UpdatedAt      sql.NullTime   `db:"updatedAt"`
+	IsBlocked      int64          `db:"is_blocked"`
+	BlockedDate    sql.NullTime   `db:"blocked_date"`
+	CreatedAt      string         `db:"created_at"`
+	UpdatedAt      sql.NullTime   `db:"updated_at"`
 }
