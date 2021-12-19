83c948f72550e8439377b7b1cf0ec6f7af084c64
imamponco <ponco@nusantarabetastudio.com>
2021-12-20 01:02:07 +0700

    [FIXED] Query for getting data address and mapping data type

--- internal/customer-svc/repository/user_external.go
+++ internal/customer-svc/repository/user_external.go
@@ -34,20 +34,20 @@ func (a *UserExternal) FindByEmailOrPhone(email string) (*model.User, error) {
 func (a *UserExternal) FindAddressByCustomerId(id string) (*model.AddressExternal, error) {
 
 	from := `user`
-	columns := `user.user_AIID, user.alamat, user.kodepos, kel.nama_kelurahan as kelurahan, kec.nama_kecamatan as kecamatan, kab.nama_kabupaten as kabupaten, prov.nama_provinsi as provinsi `
+	columns := `user.user_AIID, user.alamat, user.kodepos, kel.nama_kelurahan as kelurahan, kec.nama_kecamatan as kecamatan, kab.nama_kabupaten as kabupaten, prov.nama_provinsi as provinsi, `
 	columns += `kel.id as id_kelurahan, kec.id as id_kecamatan, kab.id as id_kabupaten, prov.id as id_provinsi`
-	joinTables := `LEFT JOIN ref_kelurahan kel ON user.id_kelurahan=kel.kode_kelurahan`
-	joinTables += `LEFT JOIN ref_kecamatan kec ON kel.kode_kecamatan=kec.kode_kecamatan`
-	joinTables += `LEFT JOIN ref_kabupaten kab ON kec.kode_kabupaten=kab.kode_kabupaten`
-	joinTables += `LEFT JOIN ref_provinsi prov ON kab.kode_provinsi=prov.kode_provinsi`
+	joinTables := `LEFT JOIN ref_kelurahan kel ON user.id_kelurahan=kel.kode_kelurahan `
+	joinTables += `LEFT JOIN ref_kecamatan kec ON kel.kode_kecamatan=kec.kode_kecamatan `
+	joinTables += `LEFT JOIN ref_kabupaten kab ON kec.kode_kabupaten=kab.kode_kabupaten `
+	joinTables += `LEFT JOIN ref_provinsi prov ON kab.kode_provinsi=prov.kode_provinsi `
 
 	q := fmt.Sprintf("SELECT %s FROM %s %s WHERE user.user_AIID = %s", columns, from, joinTables, id)
 	// Execute query
 	q = a.db.Conn.Rebind(q)
-	var row *model.AddressExternal
+	var row []model.AddressExternal
 	err := a.db.Conn.Select(&row, q)
 	if err != nil {
 		return nil, ncore.TraceError(err)
 	}
-	return row, nil
+	return &row[0], nil
 }
