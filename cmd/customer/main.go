4abcd5dc313088d47f8832f234b50d39a1269b94
Dinan R Firdaus <dinan@nusantarabetastudio.com>
2022-02-10 18:58:04 +0700

    [CHANGED] Refactor boot core and added register handlers

---
+++ cmd/customer/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/lib/pq"
+	"github.com/nbs-go/nlogger"
+	"net/http"
+	"os"
+	"repo.pegadaian.co.id/ms-pds/srv-customer/internal/customer"
+	"repo.pegadaian.co.id/ms-pds/srv-customer/internal/pkg/nucleo/ncore"
+	"time"
+)
+
+var log = nlogger.Get()
+
+func main() {
+	// Capture started at
+	startedAt := time.Now()
+
+	// Handle command flags
+	bootOptions := handleCmdFlags()
+
+	// Boot core
+	config := customer.Config{}
+	configExternal := customer.DatabaseConfig{}
+
+	c := ncore.Boot(&config, &configExternal, bootOptions.Core)
+	config.DatabaseExternal = configExternal
+
+	// Init handler
+	h, err := customer.NewHandler(c, &config)
+	if err != nil {
+		log.Debugf("error while init handler", err)
+		panic(err)
+	}
+
+	// Register handlers
+	handlers := customer.RegisterHandler(c.Manifest, h)
+
+	// Set router
+	router := customer.InitRouter(c.WorkDir, &config, handlers)
+	serverConfig := config.ServerConfig
+
+	// Start server
+	log.Infof("Starting %s...", c.Manifest.AppName)
+	log.Infof("NodeId = %s, Environment = %s", c.NodeId, c.GetEnvironmentString())
+	log.Debugf("Boot Time: %s", time.Since(startedAt))
+
+	err = http.ListenAndServe(serverConfig.GetListenPort(), router)
+	if err != nil {
+		log.Fatal("failed to serve", nlogger.Error(err))
+		os.Exit(2)
+	}
+}
--- cmd/customer/manifest.go
+++ cmd/customer/manifest.go
@@ -1,14 +1,12 @@
 package main
 
-const EnvPrefix = ""
+const EnvPrefix = "CUSTOMER_SVC"
 
 var (
-	// AppSlug Application Slug Name
-	AppSlug = "ms-customer"
 	// AppName Application Name
-	AppName = "PDS Microservice Customer"
-	// AppVersion it will be override during automated build
-	AppVersion = "0.0.0-local"
+	AppName = "Customer Service"
+	// AppVersion value will be overridden during automated build
+	AppVersion = "local"
 	// BuildSignature value will be overridden during automated build
 	BuildSignature = "local.build"
 )
