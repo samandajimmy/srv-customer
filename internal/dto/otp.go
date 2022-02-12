6185a2162d8c89348ce1e9f32b85d8ee780183ae
Dinan R Firdaus <dinan@nusantarabetastudio.com>
2022-02-12 12:52:47 +0700

    Removed unused repo create user register

--- internal/dto/otp.go
+++ internal/dto/otp.go
@@ -3,6 +3,9 @@ package dto
 type TokenResponse struct {
 	AccessToken string `json:"access_token"`
 	ExpiresIn   int64  `json:"expires_in"`
+	TokenType   string `json:"token_type"`
+	Scope       string `json:"scope"`
+	JTI         string `json:"jti"`
 }
 
 type SendOTPRequest struct {
