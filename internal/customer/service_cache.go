1d6dfc8cf2cb563faa46f905172007bd0d898126
Dinan R Firdaus <dinan@nusantarabetastudio.com>
2022-02-13 23:53:45 +0700

    Added get cache jwt functions

--- internal/customer/service_cache.go
+++ internal/customer/service_cache.go
@@ -1,5 +1,10 @@
 package customer
 
+import (
+	"fmt"
+	"repo.pegadaian.co.id/ms-pds/srv-customer/internal/customer/constant"
+)
+
 func (s *Service) CacheGet(key string) (string, error) {
 
 	result, err := s.redis.Get(key)
@@ -18,4 +23,15 @@ func (s *Service) CacheSetThenGet(key string, value string, expire int64) (strin
 	}
 
 	return result, nil
+}
+
+func (s *Service) CacheGetJwt(key string) string {
+	fullKey := fmt.Sprintf("%s:%s:%s", constant.Prefix, constant.CacheTokenJWT, key)
+
+	token, err := s.CacheGet(fullKey)
+	if err != nil {
+		return ""
+	}
+
+	return token
 }
