263fa67742e9e1e90574241f0a8c96079fddde6b
Dinan R Firdaus <dinan@nusantarabetastudio.com>
2022-03-09 22:12:36 +0700

    Added bank account statement

---
+++ internal/customer/statement/statement_bank_account.go
@@ -0,0 +1,56 @@
+package statement
+
+import (
+	"github.com/jmoiron/sqlx"
+	"github.com/nbs-go/nsql/pq/query"
+	"github.com/nbs-go/nsql/schema"
+	"repo.pegadaian.co.id/ms-pds/srv-customer/internal/customer/model"
+	"repo.pegadaian.co.id/ms-pds/srv-customer/internal/pkg/nucleo/nsql"
+)
+
+// Define schema
+
+var BankAccountSchema = schema.New(schema.FromModelRef(model.BankAccount{}))
+
+type BankAccount struct {
+	Insert              *sqlx.NamedStmt
+	Update              *sqlx.NamedStmt
+	DeleteByXID         *sqlx.Stmt
+	FindByXID           *sqlx.Stmt
+	FindAllByCustomerID *sqlx.Stmt
+}
+
+func NewBankAccount(db *nsql.DatabaseContext) *BankAccount {
+	// Init query Schema Builder
+	sb := query.Schema(BankAccountSchema)
+
+	// Init query
+	findByXID := query.Select(query.Column("*")).
+		From(BankAccountSchema).
+		Where(query.Equal(query.Column("xid"))).
+		Limit(1).
+		Build()
+
+	findAllByCustomerID := query.Select(query.Column("*")).
+		From(BankAccountSchema).
+		Where(query.Equal(query.Column("customerId"))).
+		Build()
+
+	update := query.
+		Update(
+			BankAccountSchema,
+			"version", "modifiedBy", "updatedAt", "accountName", "accountNumber", "bank", "status").
+		Build()
+
+	deleteByXID := query.Delete(BankAccountSchema).
+		Where(query.Equal(query.Column("xid"))).
+		Build()
+
+	return &BankAccount{
+		Insert:              db.PrepareNamed(sb.Insert()),
+		Update:              db.PrepareNamed(update),
+		DeleteByXID:         db.PrepareFmtRebind(deleteByXID),
+		FindByXID:           db.PrepareFmtRebind(findByXID),
+		FindAllByCustomerID: db.PrepareFmtRebind(findAllByCustomerID),
+	}
+}
