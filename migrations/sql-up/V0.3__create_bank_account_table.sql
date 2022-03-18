96e1170c4884e8e3675749d3c8f3937d381bba94
Dinan R Firdaus <dinan@nusantarabetastudio.com>
2022-03-18 10:12:38 +0700

    refactor: Fix indentation on bank account migration script

--- migrations/sql-up/V0.3__create_bank_account_table.sql
+++ migrations/sql-up/V0.3__create_bank_account_table.sql
@@ -1,17 +1,17 @@
 CREATE TABLE public."BankAccount"
 (
-    "id"            BIGSERIAL PRIMARY KEY NOT NULL,
-    "xid"           VARCHAR(64)           NOT NULL,
-    "metadata"      JSON NULL,
+    "id"            BIGSERIAL PRIMARY KEY       NOT NULL,
+    "xid"           VARCHAR(64)                 NOT NULL,
+    "metadata"      JSON                        NULL,
     "createdAt"     TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     "updatedAt"     TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-    "modifiedBy"    JSON                  NOT NULL,
-    "version"       BIGINT                NOT NULL DEFAULT 1,
-    "customerId"    BIGINT                NOT NULL,
-    "accountName"   VARCHAR               NOT NULL,
-    "accountNumber" VARCHAR               NOT NULL,
-    "bank"          JSON                  NOT NULL,
-    "status"        SMALLINT              NOT NULL
+    "modifiedBy"    JSON                        NOT NULL,
+    "version"       BIGINT                      NOT NULL DEFAULT 1,
+    "customerId"    BIGINT                      NOT NULL,
+    "accountName"   VARCHAR                     NOT NULL,
+    "accountNumber" VARCHAR                     NOT NULL,
+    "bank"          JSON                        NOT NULL,
+    "status"        SMALLINT                    NOT NULL
 );
 
 CREATE INDEX BankAccount_customerId_idx
