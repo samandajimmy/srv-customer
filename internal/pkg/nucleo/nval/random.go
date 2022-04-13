f122c934fa871477bdd33e53e21f1575d184527e
imamponco <ponco@nusantarabetastudio.com>
2022-04-13 21:33:03 +0700

    fix(register): Fix Verification Token Random String Logic

--- internal/pkg/nucleo/nval/random.go
+++ internal/pkg/nucleo/nval/random.go
@@ -6,13 +6,29 @@ import (
 	"math/rand"
 )
 
-var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
+var letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
 
-func RandomString(length int) string {
-	b := make([]rune, length)
-	for i := range b {
-		b[i] = letters[rand.Intn(len(letters))] //nolint:gosec
+const (
+	letterIdxBits = 6                    // 6 bits to represent a letter index
+	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
+)
+
+func RandomString(n int) string {
+	b := make([]byte, n)
+	// A rand.Int63() generates 63 random bits, enough for letterIdxMax letters!
+	for i, cache, remain := n-1, rand.Int63(), letterIdxMax; i >= 0; { //nolint:gosec
+		if remain == 0 {
+			cache, remain = rand.Int63(), letterIdxMax //nolint:gosec
+		}
+		if idx := int(cache & letterIdxMask); idx < len(letters) {
+			b[i] = letters[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
 	}
+
 	return string(b)
 }
 
